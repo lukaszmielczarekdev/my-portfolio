fe184d448532520a9102ffcdc6e390928093941a
lukaszmielczarekdev <lukaszmielczarekdev@gmail.com>
2022-01-24 00:16:02 +0100

    MP-2 main components structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import "./App.css";
+import { AppContainer } from "./AppStyles";
+import NavBar from "./components/NavBar/NavBar";
+import Hero from "./components/Hero/Hero";
+import Projects from "./components/Projects/Projects";
+import Tools from "./components/Tools/Tools";
+import TimeLine from "./components/TimeLine/TimeLine";
+import Footer from "./components/Footer/Footer";
 
-function App() {
-  return <div>My-portfolio</div>;
-}
+const App = () => {
+  return (
+    <AppContainer>
+      <NavBar />
+      <main>
+        <Hero />
+        <Projects />
+        <Tools />
+        <TimeLine />
+      </main>
+      <Footer />
+    </AppContainer>
+  );
+};
 
 export default App;
