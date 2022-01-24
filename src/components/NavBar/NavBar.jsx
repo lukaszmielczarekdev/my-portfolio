5e17d83cb68898c5c3f021268c747beb83d37fb2
lukaszmielczarekdev <lukaszmielczarekdev@gmail.com>
2022-01-24 20:51:51 +0100

    MP-4 navbar component (without logo)

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import {
+  Container,
+  NavSection,
+  NavigationTitle,
+  NavigationLink,
+} from "./NavBarStyles";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <Container>
+      <NavSection>
+        <NavigationTitle>Portfolio</NavigationTitle>
+      </NavSection>
+      <NavSection>
+        <NavigationLink href="#projects">Projects</NavigationLink>
+        <NavigationLink href="#technologies">Technologies</NavigationLink>
+        <NavigationLink href="#about">About</NavigationLink>
+      </NavSection>
+    </Container>
+  );
 };
 
 export default NavBar;
---
+++ src/components/NavBar/NavBarStyles.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+// Container
+export const Container = styled.div`
+  display: grid;
+  justify-content: center;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 1fr;
+  grid-column-gap: 1rem;
+  padding: 1rem;
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(2, 1fr);
+    grid-column-gap: 0.5rem;
+    grid-row-gap: 0.5rem;
+  }
+`;
+
+//NavSection
+export const NavSection = styled.div`
+  display: flex;
+  align-content: center;
+  justify-content: space-around;
+`;
+
+// NavTitle
+export const NavigationTitle = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+  color: white;
+  transition: 0.4s ease;
+  margin: 1.5rem;
+  @media ${(props) => props.theme.breakpoints.sm} {
+    margin: 1rem;
+  }
+`;
+
+// NavLink
+export const NavigationLink = styled.a`
+  font-size: 2rem;
+  font-weight: bold;
+  color: rgba(255, 255, 255, 0.7);
+  transition: 0.4s ease;
+  margin: 1.5rem;
+  &:hover {
+    color: white;
+    opacity: 1;
+    cursor: pointer;
+  }
+  @media ${(props) => props.theme.breakpoints.sm} {
+    margin: 1rem;
+  }
+`;
